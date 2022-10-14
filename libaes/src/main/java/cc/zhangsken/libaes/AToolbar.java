2a8c17a21198f8c4e2df0fc853558f209736658e
ZhanGSKen <ZhanGSKen@QQ.COM>
2022-10-14 16:12:58 +0800

    提供配置化的AToolbar

--- libaes/src/main/java/cc/zhangsken/libaes/AToolbar.java
+++ libaes/src/main/java/cc/zhangsken/libaes/AToolbar.java
@@ -1,7 +1,11 @@
 package cc.zhangsken.libaes;
+
 import android.widget.Toolbar;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 
 public class AToolbar extends Toolbar {
     
@@ -11,7 +15,26 @@ public class AToolbar extends Toolbar {
 
     public AToolbar(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setBackground(context.getDrawable(cc.zhangsken.libaes.R.drawable.frame_toolbar));
+	
+		// 获得TypedArray
+		//TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AToolbar);
+		// 获得attrs.xml里面的属性值,格式为:名称_属性名,后面是默认值
+		//int startColor = a.getColor(R.styleable.AToolbar_startColor, context.getColor(R.color.colorAToolbarStartColor));
+		//int centerColor = a.getColor(R.styleable.AToolbar_centerColor, context.getColor(R.color.colorAToolbarCenterColor));
+		//int endColor = a.getColor(R.styleable.AToolbar_endColor, context.getColor(R.color.colorAToolbarEndColor));
+		//float tSize = a.getDimension(R.styleable.CustomView_tSize, 35);
+		//p.setColor(tColor);
+		//p.setTextSize(tSize);
+		//Drawable drawable = context.getDrawable(R.drawable.frame_atoolbar);
+		
+		setBackground(context.getDrawable(R.drawable.frame_atoolbar));
+		
+		// 返回一个绑定资源结束的信号给资源
+		//a.recycle();
+
+	
+		
+        
     }
 
     public AToolbar(Context context, AttributeSet attrs, int defStyleAttr) {
