3867918ef7b0901255ca2379a068562766f0920a
ZhanGSKen <ZhanGSKen@QQ.COM>
2022-10-18 10:24:41 +0800

    ACard设计初版

---
+++ libaes/src/main/java/cc/zhangsken/libaes/ACard.java
@@ -0,0 +1,49 @@
+package cc.zhangsken.libaes;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ACard extends ViewGroup {
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        
+    }
+    
+    
+    public ACard(Context context) {
+        super(context);
+    }
+
+    public ACard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // 获得TypedArray
+        //TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AToolbar);
+        // 获得attrs.xml里面的属性值,格式为:名称_属性名,后面是默认值
+        //int startColor = a.getColor(R.styleable.AToolbar_startColor, context.getColor(R.color.colorAToolbarStartColor));
+        //int centerColor = a.getColor(R.styleable.AToolbar_centerColor, context.getColor(R.color.colorAToolbarCenterColor));
+        //int endColor = a.getColor(R.styleable.AToolbar_endColor, context.getColor(R.color.colorAToolbarEndColor));
+        //float tSize = a.getDimension(R.styleable.CustomView_tSize, 35);
+        //p.setColor(tColor);
+        //p.setTextSize(tSize);
+        //Drawable drawable = context.getDrawable(R.drawable.frame_atoolbar);
+
+        setBackground(context.getDrawable(R.drawable.acard_frame_main));
+
+        // 返回一个绑定资源结束的信号给资源
+        //a.recycle();
+
+
+
+
+    }
+
+    public ACard(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    
+}
